46862ef6843d46b516f8186cabaae016bdbb1ad9
ezgi arslan <ezgiarslan26@gmail.com>
2021-01-22 15:26:06 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T, T1>
+    {
+
+        T[] userid;
+        T1[] username;
+        T[] temp1;
+        T1[] temp2;
+
+        public MyDictionary()
+        {
+            
+            userid = new T[0];
+            username = new T1[0];
+        }
+        public void Add(T userId, T1 userName)
+        {
+
+          
+
+            temp1 = userid;
+            temp2 = username;
+
+            userid = new T[userid.Length + 1];
+            username = new T1[username.Length + 1];
+
+            for (int i = 0; i < temp1.Length; i++)
+            {
+                userid[i] = temp1[i];
+
+
+            }
+            for (int i = 0; i < temp2.Length; i++)
+            {
+                username[i] = temp2[i];
+            }
+
+
+            userid[userid.Length - 1] = userId;
+            username[username.Length - 1] = userName;
+
+
+        }
+        public int Count
+        {
+            get { return userid.Length; }
+
+
+        }
+        public T1 this[T key]
+        {
+            get
+            {
+                int index = Array.IndexOf(userid, key);
+                return username[index];
+            }
+        }
+        
+    }
+    }
---
+++ Dictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> users = new MyDictionary<int, string>();
+
+            users.Add(0, "Ezgi");
+            users.Add(1, "Furkan");
+            users.Add(2, "Melisa");
+            users.Add(3, "Elif");
+            users.Add(4, "Berkay");
+            users.Add(5, "Burcu");
+
+            Console.WriteLine(users.Count);
+
+            Console.WriteLine(users[0]);
+            Console.WriteLine(users[1]);
+            Console.WriteLine(users[2]);
+            Console.WriteLine(users[3]);
+            Console.WriteLine(users[4]);
+            Console.WriteLine(users[5]);
+
+
+
+        }
+    }
+}
